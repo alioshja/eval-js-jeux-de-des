8bfd3b7381a18270f445ed893eb4e0cfd18e415c
alioshja <laboucheriedu12cestbeau@gmail.com>
2023-09-07 18:51:09 +0200

    jeus de dés 07/09/23   18H50

--- js/script.js
+++ js/script.js
@@ -3,111 +3,139 @@
 const max = 6;
 const min = 1;
 //tTscore est la variable de mon compteur total ce qui fait que je l'ai déclaré avant la fonction pour éviter que ma variable soit redéfini a 0 a chaque utilisation de ma fonction
-var tTScore = 0;
-var tTScore2 = 0;
+let tTScore = 0;
+let tTScore2 = 0;
+
+        //liens images pour dès
+        const d1l = "images/1-des-face.png";
+        const d2l = "images/2-des-face.png";
+        const d3l = "images/3-des-face.png";
+        const d4l = "images/4-des-face.png";
+        const d5l = "images/5-des-face.png";
+        const d6l = "images/6-des-face.png";
+
+function newGame() {
+let tTScore = '0';
+let tTScore2 = '0';
+document.getElementById('SCRT1').setAttribute('text',tTScore);
+document.getElementById('SCRT2').setAttribute('text',tTScore2);
+}
+let tour = '';
+let resultat;
+function hold(joueur) {
+    tTScore = pScore; 
+    let scoreTotal = '';
+        //redefinition des var en fonction de quel utilisateur il sagit J1 ou J2 par conditions
+
+    if (joueur == 1) {
+    scoreTotal = 'SCRT1';
+    tour = 'Joueur 2 à vous';
+    resultat = [scoreTotal,tour];
+    return resultat;
+}
+else if (joueur ==2) {
+    scoreTotal = 'SCRT2'
+    tour = 'Joueur 1 à vous.'
+    resultat = [scoreTotal,tour];
+    return resultat;
+}
+else if (joueur == 3) {
+    tTScore = 0;
+    scoreTotal = 'SRT2';
+    tour = 'Joueur 2 à vous';
+    resultat = [scoreTotal,tour];
+    return resultat;
+}
+else if (joueur == 4) {
+    tTScore = 0;
+    scoreTotal = 'SRT2';
+    tour = 'Joueur 1 à vous.';
+    resultat = [scoreTotal,tour];
+    return resultat;
+}
+document.getElementById(scoreTotal).textContent = tTScore;
+console.log(resultat[2]);
+}
+let score = 0;
+let pScore = 0;
+let passerTour = '';
 function launching(joueur)
 {
+    //les joueurs peuvent lancer autant de fois le des quils le veulent mais si le dès tombe sur 1 le score tombe a 
+    //0 et c'est au tour du 2ème joueur si un joueur sauvegarde son score grace au bouton hold alor il ne peut plus perdre son score et c'est au tour du 2ème joueur.
+    
     //initialisation des var communes
-    //var tTScore = 0;
-    var pScore = 0;
-    var lancer = 1;//Math.floor(Math.random() * (max - min + 1)) + min;
-    var number = lancer;//cheked
-    document.getElementById('song').play();
-    var imgID = '';
-    var nomDeClasse = '';
-    var score = '';
-    var scoreTotal = '';
-    var tour = '';
+        let lancer = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(lancer)
+        document.getElementById('song').play();
+        const nomDeClasse = 'imgDes';
+        let imgID = 'imgS';
     //redefinition des var en fonction de quel utilisateur il sagit J1 ou J2 par conditions
     if (joueur == '1') {
-        imgID = 'imgS2';
-        nomDeClasse = 'imgDes';
+        // ID score actuel
         score = 'SCR1';
-        scoreTotal = 'SCRT1';
-        var tour = 'joueur 1 à vous.';
+        passerTour = hold(3);
     }
     else if (joueur == '2') {
-        imgID = 'imgS2';
-        nomDeClasse = 'imgDes2';
         score = 'SCR2';
-        scoreTotal = 'SCRT2';
-        var tour = 'joueur 2 à vous.';
+        passerTour = hold(4);
     }
-    else console.log('probleme');
-    var getID = document.getElementById(imgID);
-        //liens images pour dès
-var d1l = "images/1-des-face.png";
-var d2l = "images/2-des-face.png";
-var d3l = "images/3-des-face.png";
-var d4l = "images/4-des-face.png";
-var d5l = "images/5-des-face.png";
-var d6l = "images/6-des-face.png";
-
+    else ;
+    const getID = document.getElementById(imgID);
 //---------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------
     
     //algoritme qui défini quelle images charges et quel chiffre aléatoire c'est ainsi que les modifications du dom.
-    if (number == 1) {
+    if (lancer == 1) {
+        pScore = 0;
         getID.setAttribute('src',d1l);
-        getID.setAttribute('class',nomDeClasse)
-        pScore=1;
-        tTScore++;
+        getID.setAttribute('class',nomDeClasse);
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        passerTour;
+        return pScore;
     }
 
-    else if (number == 2) {
+    else if (lancer == 2) {
         getID.setAttribute('src',d2l);
         getID.setAttribute('class',nomDeClasse)
-        pScore = 2;
-        tTScore =  tTScore + 2;
+        pScore = pScore + 2;
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        return pScore;
     }
 
-    else if (number == 3) {
+    else if (lancer == 3) {
         getID.setAttribute('src',d3l);
         getID.setAttribute('class',nomDeClasse)
-        pScore = 3;
-        tTScore = tTScore + 3;
+        pScore = pScore + 3;
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        return pScore;
     }
 
-    else if (number == 4) {
+    else if (lancer == 4) {
         getID.setAttribute('src',d4l);
         getID.setAttribute('class',nomDeClasse)
-        pScore = 4;
-        tTScore = tTScore + 4;
+        pScore = pScore + 4;
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        return pScore;
     }
 
-    else if (number == 5) {    
+    else if (lancer == 5) {    
         getID.setAttribute('src',d5l);
         getID.setAttribute('class',nomDeClasse)
-        pScore = 5;
-        tTScore = tTScore + 5;
+        pScore = pScore + 5;
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        return pScore;
     }
 
-    else if (number == 6) {
+    else if (lancer == 6) {
         getID.setAttribute('src',d6l);
         getID.setAttribute('class',nomDeClasse)
-        pScore = 6;
-        tTScore = tTScore + 6;
+        pScore = pScore + 6;
         document.getElementById(score).textContent = pScore;
-        document.getElementById(scoreTotal).textContent = tTScore;
-        return tTScore;
+        return pScore;
     }
-    console.log(tTScore);
+    console.log(pScore);
     //------------------------------------------------------------------------
     //------------------------------------------------------------------------
     //------------------------------------------------------------------------
